47434d599ab0c02046129ff4e31cceb995591f18
leop <leopetit200@gmail.com>
2024-12-08 15:29:58 +0100

    oops

---
+++ back/src/main/java/com/openclassrooms/mddapi/config/SwaggerConfig.java
@@ -0,0 +1,32 @@
+package com.openclassrooms.mddapi.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    private SecurityScheme createAPIKeySecurityScheme() {
+        return new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT");
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
+                .components(new Components()
+                        .addSecuritySchemes("Bearer Authentication", createAPIKeySecurityScheme()))
+                .info(new Info()
+                        .title("MDD API Documentation")
+                        .version("v1")
+                        .description("This documentation provides information about the secured endpoints of the MDD API."));
+    }
+}
